81a7f3107a53d4b29f503b90ca3f6fade0fabb04
Abdul Ahad Khan <aakabdulahad7@gmail.com>
2024-07-28 13:42:01 +0500

    env Added

--- src/FirebaseConfig.js
+++ src/FirebaseConfig.js
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { Firestore, getFirestore } from "firebase/firestore";
+
 // import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -8,13 +9,13 @@ import { Firestore, getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCPiSfZoQTTg4gl1ybKHcz_BqS3haODl6U",
-  authDomain: "studentmanagementsystem-d311f.firebaseapp.com",
-  projectId: "studentmanagementsystem-d311f",
-  storageBucket: "studentmanagementsystem-d311f.appspot.com",
-  messagingSenderId: "112281483237",
-  appId: "1:112281483237:web:7233a028734bc0ed97610c",
-//   measurementId: "G-098Y318WNS"
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID
+  //   measurementId: "G-098Y318WNS"
 };
 
 // Initialize Firebase
@@ -22,5 +23,5 @@ const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
 
-export {db}
+export { db }
 // const analytics = getAnalytics(app);
